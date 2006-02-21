b16d34761151e7867f51fb21ef2194d0ea953843
imi <imi@0c6e7542-c601-0410-84e7-c038aed88b3b>
2006-02-21 12:39:40 +0000

    fixed bug where merged nodes and line segments were equal but not the same (aliasing)
    git-svn-id: https://josm.openstreetmap.de/svn@57 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/org/openstreetmap/josm/test/MergeVisitorTest.java
+++ test/org/openstreetmap/josm/test/MergeVisitorTest.java
@@ -6,7 +6,9 @@ import org.openstreetmap.josm.data.GeoPoint;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.LineSegment;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.visitor.MergeVisitor;
+import org.openstreetmap.josm.test.framework.Bug;
 import org.openstreetmap.josm.test.framework.DataSetTestCaseHelper;
 
 public class MergeVisitorTest extends TestCase {
@@ -18,19 +20,57 @@ public class MergeVisitorTest extends TestCase {
 	 */
 	public void testMergeOldLineSegmentsWithNew() {
 		DataSet ds = new DataSet();
-		Node n1 = DataSetTestCaseHelper.createNode(ds);
-		n1.id = 1;
-		Node n2 = DataSetTestCaseHelper.createNode(ds);
-		n2.id = 2;
-		LineSegment ls1 = DataSetTestCaseHelper.createLineSegment(ds, n1, n2);
+		Node[] n = createNodes(ds, 2);
+		LineSegment ls1 = DataSetTestCaseHelper.createLineSegment(ds, n[0], n[1]);
 		ls1.id = 3;
 
 		Node newnode = new Node();
-		newnode.coor = new GeoPoint(n2.coor.lat, n2.coor.lon);
-		LineSegment newls = new LineSegment(n1, newnode);
+		newnode.coor = new GeoPoint(n[1].coor.lat, n[1].coor.lon);
+		LineSegment newls = new LineSegment(n[0], newnode);
 
 		MergeVisitor v = new MergeVisitor(ds);
 		v.visit(newls);
 		assertEquals("line segment should have been merged.", 1, ds.lineSegments.size());
+	}
+	
+	/**
+	 * Nodes beeing merged are equal but not the same.
+	 */
+	@Bug(54)
+	public void testEqualNotSame() {
+		// create a dataset with line segment a-b
+		DataSet ds = new DataSet();
+		Node n[] = createNodes(ds, 2);
+		LineSegment ls1 = DataSetTestCaseHelper.createLineSegment(ds, n[0], n[1]);
+		ls1.id = 1;
+		
+		// create an other dataset with line segment a'-c (a' is equal, but not same to a)
+		DataSet ds2 = new DataSet();
+		Node n2[] = createNodes(ds2, 2);
+		n2[0].coor = new GeoPoint(n[0].coor.lat, n[0].coor.lon);
+		n2[1].id = 42;
+		LineSegment ls2 = DataSetTestCaseHelper.createLineSegment(ds, n2[0], n2[1]);
+		
+		MergeVisitor v = new MergeVisitor(ds);
+		for (OsmPrimitive osm : ds2.allPrimitives())
+			osm.visit(v);
+		v.fixReferences();
+		
+		assertSame(ls1.start, ls2.start);
+	}
+	
+	
+	/**
+	 * Create that amount of nodes and add them to the dataset. The id will be 1,2,3,4...
+	 * @param amount Number of nodes to create.
+	 * @return The created nodes.
+	 */
+	private Node[] createNodes(DataSet ds, int amount) {
+		Node[] nodes = new Node[amount];
+		for (int i = 0; i < amount; ++i) {
+			nodes[i] = DataSetTestCaseHelper.createNode(ds);
+			nodes[i].id = i+1;
+		}
+		return nodes;
 	}
 }
