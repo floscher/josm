a83054cff79f2a3c9a217c488e63c55fc8ed2580
imi <imi@0c6e7542-c601-0410-84e7-c038aed88b3b>
2006-03-27 06:24:41 +0000

    fixed incomplete line segment saving
    git-svn-id: https://josm.openstreetmap.de/svn@72 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/tools/XmlWriter.java
@@ -0,0 +1,50 @@
+package org.openstreetmap.josm.tools;
+
+import java.util.HashMap;
+
+/**
+ * Helper class to use for xml outputting classes.
+ * 
+ * @author imi
+ */
+public class XmlWriter {
+
+	/**
+	 * Encode the given string in XML1.0 format.
+	 * Optimized to fast pass strings that don't need encoding (normal case).
+	 */
+	public static String encode(String unencoded) {
+		StringBuilder buffer = null;
+		for (int i = 0; i < unencoded.length(); ++i) {
+			String encS = XmlWriter.encoding.get(unencoded.charAt(i));
+			if (encS != null) {
+				if (buffer == null)
+					buffer = new StringBuilder(unencoded.substring(0,i));
+				buffer.append(encS);
+			} else if (buffer != null)
+				buffer.append(unencoded.charAt(i));
+		}
+		return (buffer == null) ? unencoded : buffer.toString();
+	}
+
+	/**
+	 * @return The standard XML1.0 header. Encoding is utf-8
+	 */
+	public static Object header() {
+		return "<?xml version='1.0' encoding='UTF-8'?>";
+	}
+
+	
+	
+	final private static HashMap<Character, String> encoding = new HashMap<Character, String>();
+	static {
+		encoding.put('<', "&lt;");
+		encoding.put('>', "&gt;");
+		encoding.put('"', "&quot;");
+		encoding.put('\'', "&apos;");
+		encoding.put('&', "&amp;");
+		encoding.put('\n', "&#xA;");
+		encoding.put('\r', "&#xD;");
+		encoding.put('\t', "&#x9;");
+	}
+}
