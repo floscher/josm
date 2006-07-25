a0867b3ca0788447192dab9c3412cf325b6d396b
imi <imi@0c6e7542-c601-0410-84e7-c038aed88b3b>
2006-07-25 16:53:18 +0000

    fixed annotation presets view (added a new renderer)
    git-svn-id: https://josm.openstreetmap.de/svn@126 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/dialogs/PropertiesDialog.java
+++ src/org/openstreetmap/josm/gui/dialogs/PropertiesDialog.java
@@ -27,10 +27,13 @@ import java.util.Vector;
 import java.util.Map.Entry;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -259,6 +262,27 @@ public class PropertiesDialog extends ToggleDialog implements SelectionChangedLi
 				}
 				annotationPresets.setSelectedIndex(0);
 			}
+		});
+		annotationPresets.setRenderer(new DefaultListCellRenderer(){
+			@Override public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+	            AnnotationPreset a = (AnnotationPreset)value;
+				if (a.name == null)
+	            	return super.getListCellRendererComponent(list, "", index, false, false);
+				JComponent c = (JComponent)super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+	            JLabel l = new JLabel((a).name);
+	            l.setForeground(c.getForeground());
+	            l.setBackground(c.getBackground());
+	            l.setFont(c.getFont());
+	            l.setBorder(c.getBorder());
+	            if (a.types == null)
+	            	l.setIcon(ImageProvider.get("data", "empty"));
+	            else if (a.types.size() != 1)
+	            	l.setIcon(ImageProvider.get("data", "object"));
+	            else
+	            	l.setIcon(ImageProvider.get("data", a.types.iterator().next().getSimpleName().toLowerCase()));
+	            l.setOpaque(true);
+	            return l;
+            }
 		});
 
 		data.setColumnIdentifiers(new String[]{tr("Key"),tr("Value")});
