7317cf53893e8dc9d01d79bfbb382cd17592e72b
framm <framm@0c6e7542-c601-0410-84e7-c038aed88b3b>
2007-04-27 19:27:20 +0000

    Speed up rendering by determining crucial preference settings once instead of for every segment drawn. Patch by eai@opencoding.net.
    git-svn-id: https://josm.openstreetmap.de/svn@221 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/osm/visitor/SimplePaintVisitor.java
+++ src/org/openstreetmap/josm/data/osm/visitor/SimplePaintVisitor.java
@@ -3,6 +3,8 @@ package org.openstreetmap.josm.data.osm.visitor;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.osm.DataSet;
@@ -67,7 +69,7 @@ public class SimplePaintVisitor implements Visitor {
 	 * White if selected (as always) or green otherwise.
 	 */
 	public void visit(Segment ls) {
-		drawSegment(ls, getPreferencesColor("segment", darkgreen));
+		drawSegment(ls, getPreferencesColor("segment", darkgreen), Main.pref.getBoolean("draw.segment.direction"));
 	}
 
 	/**
@@ -84,12 +86,14 @@ public class SimplePaintVisitor implements Visitor {
 			}
 		}
 
+		boolean showDirectionArrow = Main.pref.getBoolean("draw.segment.direction");
+		boolean showOrderNumber = Main.pref.getBoolean("draw.segment.order_number");
 		int orderNumber = 0;
 		for (Segment ls : w.segments) {
 			orderNumber++;
 			if (!ls.selected) // selected already in good color
-				drawSegment(ls, w.selected ? getPreferencesColor("selected", Color.WHITE) : wayColor);
-			if (!ls.incomplete && Main.pref.getBoolean("draw.segment.order_number"))
+				drawSegment(ls, w.selected ? getPreferencesColor("selected", Color.WHITE) : wayColor, showDirectionArrow);
+			if (!ls.incomplete && showOrderNumber)
 				drawOrderNumber(ls, orderNumber);
 		}
 	}
@@ -103,11 +107,15 @@ public class SimplePaintVisitor implements Visitor {
 		Point p2 = nc.getPoint(ls.to.eastNorth);
 		int x = (p1.x+p2.x)/2 - 4*strlen;
 		int y = (p1.y+p2.y)/2 + 4;
-		Color c = g.getColor();
-		g.setColor(Color.black);
-		g.fillRect(x-1,y-12,8*strlen+1,14);
-		g.setColor(c);
-		g.drawString(""+orderNumber,x,y);
+
+		Rectangle screen = g.getClipBounds();
+		if (screen.contains(x,y)) {
+			Color c = g.getColor();
+			g.setColor(getPreferencesColor("background", Color.BLACK));
+			g.fillRect(x-1, y-12, 8*strlen+1, 14);
+			g.setColor(c);
+			g.drawString(""+orderNumber, x, y);
+		}
     }
 
 	/**
@@ -119,13 +127,16 @@ public class SimplePaintVisitor implements Visitor {
 	public void drawNode(Node n, Color color) {
 		Point p = nc.getPoint(n.eastNorth);
 		g.setColor(color);
-		g.drawRect(p.x-1, p.y-1, 2, 2);
+		Rectangle screen = g.getClipBounds();
+
+		if ( screen.contains(p.x, p.y) )
+			g.drawRect(p.x-1, p.y-1, 2, 2);
 	}
 
 	/**
 	 * Draw a line with the given color.
 	 */
-	protected void drawSegment(Segment ls, Color col) {
+	protected void drawSegment(Segment ls, Color col, boolean showDirection) {
 		if (ls.incomplete)
 			return;
 		if (ls.selected)
@@ -133,12 +144,18 @@ public class SimplePaintVisitor implements Visitor {
 		g.setColor(col);
 		Point p1 = nc.getPoint(ls.from.eastNorth);
 		Point p2 = nc.getPoint(ls.to.eastNorth);
-		g.drawLine(p1.x, p1.y, p2.x, p2.y);
-
-		if (Main.pref.getBoolean("draw.segment.direction")) {
-			double t = Math.atan2(p2.y-p1.y, p2.x-p1.x) + Math.PI;
-	        g.drawLine(p2.x,p2.y, (int)(p2.x + 10*Math.cos(t-PHI)), (int)(p2.y + 10*Math.sin(t-PHI)));
-	        g.drawLine(p2.x,p2.y, (int)(p2.x + 10*Math.cos(t+PHI)), (int)(p2.y + 10*Math.sin(t+PHI)));
+		
+		Rectangle screen = g.getClipBounds();
+		Line2D line = new Line2D.Double(p1.x, p1.y, p2.x, p2.y);
+		if (screen.contains(p1.x, p1.y, p2.x, p2.y) || screen.intersectsLine(line))
+		{
+			g.drawLine(p1.x, p1.y, p2.x, p2.y);
+	
+			if (showDirection) {
+				double t = Math.atan2(p2.y-p1.y, p2.x-p1.x) + Math.PI;
+		        g.drawLine(p2.x,p2.y, (int)(p2.x + 10*Math.cos(t-PHI)), (int)(p2.y + 10*Math.sin(t-PHI)));
+		        g.drawLine(p2.x,p2.y, (int)(p2.x + 10*Math.cos(t+PHI)), (int)(p2.y + 10*Math.sin(t+PHI)));
+			}
 		}
 	}
 
