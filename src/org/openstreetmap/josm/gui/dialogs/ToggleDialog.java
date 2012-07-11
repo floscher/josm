floscher/josm

commit c0bca8ddad3cb84229284b3687376a6e9837f09e

Permit multiple rows of `SideButton`s in toggle dialogs.
git-svn-id: https://josm.openstreetmap.de/svn/trunk@5322 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/ToggleDialog.java
@@ -754,30 +754,39 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
     protected void stateChanged() {
     }
 
+    @SuppressWarnings("unchecked")
     protected Component createLayout(Component data, boolean scroll, Collection<SideButton> buttons) {
-        if(scroll) {
+        return createLayout(data, scroll, new Collection[]{buttons});
+    }
+
+    protected Component createLayout(Component data, boolean scroll, Collection<SideButton>... buttons) {
+        if (scroll) {
             data = new JScrollPane(data);
         }
         add(data, BorderLayout.CENTER);
-        if(buttons != null && buttons.size() != 0) {
-            buttonsPanel = new JPanel(Main.pref.getBoolean("dialog.align.left", false)
-                    ? new FlowLayout(FlowLayout.LEFT) : new GridLayout(1,buttons.size()));
-            for(SideButton button : buttons) {
-                buttonsPanel.add(button);
-                javax.swing.Action action = button.getAction();
-                if (action != null) {
-                    buttonActions.add(action);
-                } else {
-                    System.err.println("Button " + button + " doesn't have action defined");
-                    new Exception().printStackTrace();
+        if (buttons != null && buttons.length > 0 && !buttons[0].isEmpty()) {
+            buttonsPanel = new JPanel(new GridLayout(buttons.length, 1));
+            for (Collection<SideButton> buttonRow : buttons) {
+                final JPanel buttonRowPanel = new JPanel(Main.pref.getBoolean("dialog.align.left", false)
+                        ? new FlowLayout(FlowLayout.LEFT) : new GridLayout(1, buttonRow.size()));
+                buttonsPanel.add(buttonRowPanel);
+                for (SideButton button : buttonRow) {
+                    buttonRowPanel.add(button);
+                    javax.swing.Action action = button.getAction();
+                    if (action != null) {
+                        buttonActions.add(action);
+                    } else {
+                        System.err.println("Button " + button + " doesn't have action defined");
+                        new Exception().printStackTrace();
+                    }
                 }
             }
             add(buttonsPanel, BorderLayout.SOUTH);
-            if(Main.pref.getBoolean("dialog.dynamic.buttons", true)) {
+            if (Main.pref.getBoolean("dialog.dynamic.buttons", true)) {
                 Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.MOUSE_MOTION_EVENT_MASK);
                 buttonsPanel.setVisible(buttonHiding == ButtonHiddingType.ALWAYS_SHOWN || !isDocked);
             }
-        } else if(buttonsHide != null) {
+        } else if (buttonsHide != null) {
             buttonsHide.setVisible(false);
         }
         return data;

commit 157234a218978c771764a208ba31cb8df8aa7d0d

fix NPE bug introduced in r5322
git-svn-id: https://josm.openstreetmap.de/svn/trunk@5325 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/ToggleDialog.java
@@ -764,7 +764,7 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
             data = new JScrollPane(data);
         }
         add(data, BorderLayout.CENTER);
-        if (buttons != null && buttons.length > 0 && !buttons[0].isEmpty()) {
+        if (buttons != null && buttons.length > 0 && buttons[0] != null && !buttons[0].isEmpty()) {
             buttonsPanel = new JPanel(new GridLayout(buttons.length, 1));
             for (Collection<SideButton> buttonRow : buttons) {
                 final JPanel buttonRowPanel = new JPanel(Main.pref.getBoolean("dialog.align.left", false)