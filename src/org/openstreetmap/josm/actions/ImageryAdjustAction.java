floscher/josm

commit 6fcb5a9580e4f52f5cc22b4496bcb8c4aed0b4f6

ParallelWayAction: handle nautical mile SoM + listen to preferences changes
git-svn-id: https://josm.openstreetmap.de/svn/trunk@5982 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/mapmode/ParallelWayAction.java
@@ -23,6 +23,8 @@ import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.Preferences.PreferenceChangeEvent;
+import org.openstreetmap.josm.data.Preferences.PreferenceChangedListener;
 import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
@@ -78,9 +80,7 @@ import org.openstreetmap.josm.tools.Shortcut;
  *
  * @author Ole Jørgen Brønner (olejorgenb)
  */
-public class ParallelWayAction extends MapMode implements AWTEventListener, MapViewPaintable {
-
-    private static final long serialVersionUID = 1L;
+public class ParallelWayAction extends MapMode implements AWTEventListener, MapViewPaintable, PreferenceChangedListener {
 
     private enum Mode {
         dragging, normal
@@ -99,6 +99,7 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
     private double snapDistanceMetric;
     private double snapDistanceImperial;
     private double snapDistanceChinese;
+    private double snapDistanceNautical;
 
     private ModifiersSpec snapModifierCombo;
     private ModifiersSpec copyTagsModifierCombo;
@@ -118,7 +119,7 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
 
     private WaySegment referenceSegment;
     private ParallelWays pWays;
-    LinkedHashSet<Way> sourceWays;
+    private LinkedHashSet<Way> sourceWays;
     private EastNorth helperLineStart;
     private EastNorth helperLineEnd;
 
@@ -130,6 +131,7 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
         putValue("help", ht("/Action/Parallel"));
         mv = mapFrame.mapView;
         updateModeLocalPreferences();
+        Main.pref.addPreferenceChangeListener(this);
     }
 
     @Override
@@ -198,7 +200,6 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
 
     private void updateModeLocalPreferences() {
         // @formatter:off
-        //snapThreshold        = Main.pref.getDouble (prefKey("snap-threshold"), 0.35); // Old preference was stored in meters, hence the new name (percent)
         snapThreshold        = Main.pref.getDouble (prefKey("snap-threshold-percent"), 0.70);
         snapDefault          = Main.pref.getBoolean(prefKey("snap-default"),      true);
         copyTagsDefault      = Main.pref.getBoolean(prefKey("copy-tags-default"), true);
@@ -206,6 +207,7 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
         snapDistanceMetric   = Main.pref.getDouble(prefKey("snap-distance-metric"), 0.5);
         snapDistanceImperial = Main.pref.getDouble(prefKey("snap-distance-imperial"), 1);
         snapDistanceChinese  = Main.pref.getDouble(prefKey("snap-distance-chinese"), 1);
+        snapDistanceNautical = Main.pref.getDouble(prefKey("snap-distance-nautical"), 0.1);
 
         snapModifierCombo           = new ModifiersSpec(getStringPref("snap-modifier-combo",             "?sC"));
         copyTagsModifierCombo       = new ModifiersSpec(getStringPref("copy-tags-modifier-combo",        "As?"));
@@ -399,7 +401,7 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
             setMode(Mode.dragging);
         }
 
-        //// Calculate distance to the reference line
+        // Calculate distance to the reference line
         EastNorth enp = mv.getEastNorth((int) p.getX(), (int) p.getY());
         EastNorth nearestPointOnRefLine = Geometry.closestPointToLine(referenceSegment.getFirstNode().getEastNorth(),
                 referenceSegment.getSecondNode().getEastNorth(), enp);
@@ -422,6 +424,8 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
                 snapDistance = snapDistanceChinese * NavigatableComponent.CHINESE_SOM.aValue;
             } else if (som.equals(NavigatableComponent.IMPERIAL_SOM)) {
                 snapDistance = snapDistanceImperial * NavigatableComponent.IMPERIAL_SOM.aValue;
+            } else if (som.equals(NavigatableComponent.NAUTICAL_MILE_SOM)) {
+                snapDistance = snapDistanceNautical * NavigatableComponent.NAUTICAL_MILE_SOM.aValue;
             } else {
                 snapDistance = snapDistanceMetric; // Metric system by default
             }
@@ -575,5 +579,12 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
 
     private String getStringPref(String subKey) {
         return getStringPref(subKey, null);
+    }
+
+    @Override
+    public void preferenceChanged(PreferenceChangeEvent e) {
+        if (e.getKey().startsWith(prefKey(""))) {
+            updateAllPreferences();
+        }
     }
 }

commit 0bbb8f5f481f7968b66fda2fa8c52f1189115de2

fix #8919 - fix memory leaks, including one introduced with r5982
git-svn-id: https://josm.openstreetmap.de/svn/trunk@6107 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/ImageryAdjustAction.java
@@ -31,7 +31,6 @@ import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.gui.widgets.JosmTextField;
 
-
 public class ImageryAdjustAction extends MapMode implements MouseListener, MouseMotionListener, AWTEventListener{
     static ImageryOffsetDialog offsetDialog;
     static Cursor cursor = ImageProvider.getCursor("normal", "move");
@@ -57,16 +56,20 @@ public class ImageryAdjustAction extends MapMode implements MouseListener, Mouse
         if (!layer.isVisible()) {
             layer.setVisible(true);
         }
-        Main.map.mapView.addMouseListener(this);
-        Main.map.mapView.addMouseMotionListener(this);
         oldDx = layer.getDx();
         oldDy = layer.getDy();
+        addListeners();
+        offsetDialog = new ImageryOffsetDialog();
+        offsetDialog.setVisible(true);
+    }
+    
+    protected void addListeners() {
+        Main.map.mapView.addMouseListener(this);
+        Main.map.mapView.addMouseMotionListener(this);
         try {
             Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.KEY_EVENT_MASK);
         } catch (SecurityException ex) {
         }
-        offsetDialog = new ImageryOffsetDialog();
-        offsetDialog.setVisible(true);
     }
 
     @Override public void exitMode() {
@@ -76,12 +79,18 @@ public class ImageryAdjustAction extends MapMode implements MouseListener, Mouse
             offsetDialog.setVisible(false);
             offsetDialog = null;
         }
+        removeListeners();
+    }
+    
+    protected void removeListeners() {
         try {
             Toolkit.getDefaultToolkit().removeAWTEventListener(this);
         } catch (SecurityException ex) {
         }
-        Main.map.mapView.removeMouseListener(this);
-        Main.map.mapView.removeMouseMotionListener(this);
+        if (Main.isDisplayingMapView()) {
+            Main.map.mapView.removeMouseMotionListener(this);
+            Main.map.mapView.removeMouseListener(this);
+        }
     }
 
     @Override
@@ -263,5 +272,13 @@ public class ImageryAdjustAction extends MapMode implements MouseListener, Mouse
                 Main.map.selectSelectTool(false);
             }
         }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        removeListeners();
+        this.layer = null;
+        this.oldMapMode = null;
     }
 }

src/org/openstreetmap/josm/actions/mapmode/ParallelWayAction.java
@@ -595,4 +595,10 @@ public class ParallelWayAction extends MapMode implements AWTEventListener, MapV
             updateAllPreferences();
         }
     }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        Main.pref.removePreferenceChangeListener(this);
+    }
 }

src/org/openstreetmap/josm/gui/MapView.java
@@ -209,7 +209,7 @@ public class MapView extends NavigatableComponent implements PropertyChangeListe
      */
     public MouseEvent lastMEvent = new MouseEvent(this, 0, 0, 0, 0, 0, 0, false); // In case somebody reads it before first mouse move
 
-    private LinkedList<MapViewPaintable> temporaryLayers = new LinkedList<MapViewPaintable>();
+    private final LinkedList<MapViewPaintable> temporaryLayers = new LinkedList<MapViewPaintable>();
 
     private BufferedImage nonChangedLayersBuffer;
     private BufferedImage offscreenBuffer;
@@ -953,6 +953,12 @@ public class MapView extends NavigatableComponent implements PropertyChangeListe
         if (mapMover != null) {
             mapMover.destroy();
         }
+        activeLayer = null;
+        changedLayer = null;
+        editLayer = null;
+        layers.clear();
+        nonChangedLayers.clear();
+        temporaryLayers.clear();
     }
 
     @Override