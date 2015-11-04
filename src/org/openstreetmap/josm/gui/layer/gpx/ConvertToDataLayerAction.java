floscher/josm

commit 9784408b563ed893afe1e1e0f8a1253f678371ea

fix #9012 - Saving a converted osm/gpx layer should save with correct file extension preselected
git-svn-id: https://josm.openstreetmap.de/svn/trunk@8830 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/gpx/ConvertToDataLayerAction.java
@@ -6,6 +6,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -127,7 +128,8 @@ public abstract class ConvertToDataLayerAction<T extends Layer> extends Abstract
             return;
         }
         final DataSet ds = convert();
-        final OsmDataLayer layer = new OsmDataLayer(ds, tr("Converted from: {0}", this.layer.getName()), this.layer.getAssociatedFile());
+        final OsmDataLayer layer = new OsmDataLayer(ds, tr("Converted from: {0}", this.layer.getName()), null);
+        layer.setAssociatedFile(new File(this.layer.getAssociatedFile().getParentFile(), this.layer.getAssociatedFile().getName() + ".osm"));
         layer.setUploadDiscouraged(true);
         Main.main.addLayer(layer);
         Main.main.removeLayer(this.layer);

src/org/openstreetmap/josm/gui/layer/OsmDataLayer.java
@@ -742,6 +742,7 @@ public class OsmDataLayer extends AbstractModifiableLayer implements Listener, S
         public void actionPerformed(ActionEvent e) {
             final GpxData data = toGpxData();
             final GpxLayer gpxLayer = new GpxLayer(data, tr("Converted from: {0}", getName()));
+            gpxLayer.setAssociatedFile(new File(getAssociatedFile().getParentFile(), getAssociatedFile().getName() + ".gpx"));
             Main.main.addLayer(gpxLayer);
             if (Main.pref.getBoolean("marker.makeautomarkers", true) && !data.waypoints.isEmpty()) {
                 Main.main.addLayer(new MarkerLayer(data, tr("Converted from: {0}", getName()), null, gpxLayer));

commit 38a0445f264abe977c9d697b0c52be55785c0e5e

fix #12059 - NPE when converting GPX layer to data-layer from session.joz
Regression from r8830.

git-svn-id: https://josm.openstreetmap.de/svn/trunk@8987 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/gpx/ConvertToDataLayerAction.java
@@ -129,7 +129,9 @@ public abstract class ConvertToDataLayerAction<T extends Layer> extends Abstract
         }
         final DataSet ds = convert();
         final OsmDataLayer layer = new OsmDataLayer(ds, tr("Converted from: {0}", this.layer.getName()), null);
-        layer.setAssociatedFile(new File(this.layer.getAssociatedFile().getParentFile(), this.layer.getAssociatedFile().getName() + ".osm"));
+        if (this.layer.getAssociatedFile() != null) {
+            layer.setAssociatedFile(new File(this.layer.getAssociatedFile().getParentFile(), this.layer.getAssociatedFile().getName() + ".osm"));
+        }
         layer.setUploadDiscouraged(true);
         Main.main.addLayer(layer);
         Main.main.removeLayer(this.layer);