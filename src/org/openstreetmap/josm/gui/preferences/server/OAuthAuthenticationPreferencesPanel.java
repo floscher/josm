floscher/josm

commit 7cfdb7b92c2edcb4891a6854d7d12899893bb5b5

sonar - squid:AssignmentInSubExpressionCheck - Assignments should not be made from within sub-expressions
git-svn-id: https://josm.openstreetmap.de/svn/trunk@10179 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/preferences/server/OAuthAuthenticationPreferencesPanel.java
@@ -45,13 +45,13 @@ import org.openstreetmap.josm.tools.UserCancelException;
  * @since 2745
  */
 public class OAuthAuthenticationPreferencesPanel extends JPanel implements PropertyChangeListener {
-    private JPanel pnlAuthorisationMessage;
-    private NotYetAuthorisedPanel pnlNotYetAuthorised;
-    private AlreadyAuthorisedPanel pnlAlreadyAuthorised;
-    private AdvancedOAuthPropertiesPanel pnlAdvancedProperties;
+    private final JPanel pnlAuthorisationMessage = new JPanel(new BorderLayout());
+    private final NotYetAuthorisedPanel pnlNotYetAuthorised = new NotYetAuthorisedPanel();
+    private final AlreadyAuthorisedPanel pnlAlreadyAuthorised = new AlreadyAuthorisedPanel();
+    private final AdvancedOAuthPropertiesPanel pnlAdvancedProperties = new AdvancedOAuthPropertiesPanel();
     private String apiUrl;
-    private JCheckBox cbShowAdvancedParameters;
-    private JCheckBox cbSaveToPreferences;
+    private final JCheckBox cbShowAdvancedParameters = new JCheckBox();
+    private final JCheckBox cbSaveToPreferences = new JCheckBox(tr("Save to preferences"));
 
     /**
      * Create the panel
@@ -74,7 +74,7 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
         gc.fill = GridBagConstraints.HORIZONTAL;
         gc.weightx = 0.0;
         gc.insets = new Insets(0, 0, 0, 3);
-        pnl.add(cbShowAdvancedParameters = new JCheckBox(), gc);
+        pnl.add(cbShowAdvancedParameters, gc);
         cbShowAdvancedParameters.setSelected(false);
         cbShowAdvancedParameters.addItemListener(
                 new ItemListener() {
@@ -97,7 +97,7 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
         gc.fill = GridBagConstraints.BOTH;
         gc.weightx = 1.0;
         gc.weighty = 1.0;
-        pnl.add(pnlAdvancedProperties = new AdvancedOAuthPropertiesPanel(), gc);
+        pnl.add(pnlAdvancedProperties, gc);
         pnlAdvancedProperties.initFromPreferences(Main.pref);
         pnlAdvancedProperties.setBorder(
                 BorderFactory.createCompoundBorder(
@@ -125,12 +125,7 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
         gc.weighty = 1.0;
         gc.weightx = 1.0;
         gc.insets = new Insets(10, 0, 0, 0);
-        add(pnlAuthorisationMessage = new JPanel(new BorderLayout()), gc);
-
-        // create these two panels, they are going to be used later in refreshView
-        //
-        pnlAlreadyAuthorised = new AlreadyAuthorisedPanel();
-        pnlNotYetAuthorised = new NotYetAuthorisedPanel();
+        add(pnlAuthorisationMessage, gc);
     }
 
     protected void refreshView() {
@@ -195,9 +190,9 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
             gc.insets = new Insets(0, 0, 3, 0);
             gc.fill = GridBagConstraints.HORIZONTAL;
             gc.weightx = 1.0;
-            JMultilineLabel lbl;
-            add(lbl = new JMultilineLabel(
-                    tr("You do not have an Access Token yet to access the OSM server using OAuth. Please authorize first.")), gc);
+            JMultilineLabel lbl = new JMultilineLabel(
+                    tr("You do not have an Access Token yet to access the OSM server using OAuth. Please authorize first."));
+            add(lbl, gc);
             lbl.setFont(lbl.getFont().deriveFont(Font.PLAIN));
 
             // Action for authorising now
@@ -220,8 +215,8 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
      *
      */
     private class AlreadyAuthorisedPanel extends JPanel {
-        private JosmTextField tfAccessTokenKey;
-        private JosmTextField tfAccessTokenSecret;
+        private final JosmTextField tfAccessTokenKey = new JosmTextField();
+        private final JosmTextField tfAccessTokenSecret = new JosmTextField();
 
         /**
          * Constructs a new {@code AlreadyAuthorisedPanel}.
@@ -239,8 +234,8 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
             gc.fill = GridBagConstraints.HORIZONTAL;
             gc.weightx = 1.0;
             gc.gridwidth = 2;
-            JMultilineLabel lbl;
-            add(lbl = new JMultilineLabel(tr("You already have an Access Token to access the OSM server using OAuth.")), gc);
+            JMultilineLabel lbl = new JMultilineLabel(tr("You already have an Access Token to access the OSM server using OAuth."));
+            add(lbl, gc);
             lbl.setFont(lbl.getFont().deriveFont(Font.PLAIN));
 
             // -- access token key
@@ -252,7 +247,7 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
 
             gc.gridx = 1;
             gc.weightx = 1.0;
-            add(tfAccessTokenKey = new JosmTextField(), gc);
+            add(tfAccessTokenKey, gc);
             tfAccessTokenKey.setEditable(false);
 
             // -- access token secret
@@ -264,7 +259,7 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
 
             gc.gridx = 1;
             gc.weightx = 1.0;
-            add(tfAccessTokenSecret = new JosmTextField(), gc);
+            add(tfAccessTokenSecret, gc);
             tfAccessTokenSecret.setEditable(false);
 
             // -- access token secret
@@ -272,7 +267,7 @@ public class OAuthAuthenticationPreferencesPanel extends JPanel implements Prope
             gc.gridx = 0;
             gc.gridwidth = 2;
             gc.weightx = 1.0;
-            add(cbSaveToPreferences = new JCheckBox(tr("Save to preferences")), gc);
+            add(cbSaveToPreferences, gc);
             cbSaveToPreferences.setSelected(OAuthAccessTokenHolder.getInstance().isSaveToPreferences());
 
             // -- action buttons

src/org/openstreetmap/josm/actions/mapmode/DrawAction.java
@@ -1664,7 +1664,8 @@ public class DrawAction extends MapMode implements MapViewPaintable, SelectionCh
                     }
                 }
             }
-            return projected = new EastNorth(e0+l*pe, n0+l*pn);
+            projected = new EastNorth(e0+l*pe, n0+l*pn);
+            return projected;
         }
 
         public void noSnapNow() {

src/org/openstreetmap/josm/actions/mapmode/SelectAction.java
@@ -724,8 +724,8 @@ public class SelectAction extends MapMode implements ModifierListener, KeyPressR
                 ((MoveCommand) c).saveCheckpoint();
                 ((MoveCommand) c).applyVectorTo(currentEN);
             } else {
-                Main.main.undoRedo.add(
-                        c = new MoveCommand(selection, startEN, currentEN));
+                c = new MoveCommand(selection, startEN, currentEN);
+                Main.main.undoRedo.add(c);
             }
             for (Node n : affectedNodes) {
                 LatLon ll = n.getCoor();
@@ -833,7 +833,7 @@ public class SelectAction extends MapMode implements ModifierListener, KeyPressR
             if (osm instanceof Way) {
                 limit -= ((Way) osm).getNodes().size();
             }
-            if ((limit -= 1) < 0) {
+            if (--limit < 0) {
                 break;
             }
         }
@@ -1199,8 +1199,10 @@ public class SelectAction extends MapMode implements ModifierListener, KeyPressR
                 for (WaySegment ws : mv.getNearestWaySegments(p, mv.isSelectablePredicate)) {
                     w = ws.way;
 
-                    Point2D p1 = mv.getPoint2D(wnp.a = w.getNode(ws.lowerIndex));
-                    Point2D p2 = mv.getPoint2D(wnp.b = w.getNode(ws.lowerIndex + 1));
+                    wnp.a = w.getNode(ws.lowerIndex);
+                    wnp.b = w.getNode(ws.lowerIndex + 1);
+                    Point2D p1 = mv.getPoint2D(wnp.a);
+                    Point2D p2 = mv.getPoint2D(wnp.b);
                     if (WireframeMapRenderer.isLargeSegment(p1, p2, virtualSpace)) {
                         Point2D pc = new Point2D.Double((p1.getX() + p2.getX()) / 2, (p1.getY() + p2.getY()) / 2);
                         if (p.distanceSq(pc) < virtualSnapDistSq2) {

src/org/openstreetmap/josm/data/CustomConfigurator.java
@@ -202,25 +202,18 @@ public final class CustomConfigurator {
      * @return number of pressed button, -1 if cancelled
      */
     public static int askForOption(String text, String opts) {
-        Integer answer;
         if (!opts.isEmpty()) {
-            String[] options = opts.split(";");
-            answer = JOptionPane.showOptionDialog(Main.parent, text, "Question",
-                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, 0);
+            return JOptionPane.showOptionDialog(Main.parent, text, "Question",
+                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, opts.split(";"), 0);
         } else {
-            answer = JOptionPane.showOptionDialog(Main.parent, text, "Question",
+            return JOptionPane.showOptionDialog(Main.parent, text, "Question",
                     JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, 2);
         }
-        if (answer == null) return -1; else return answer;
     }
 
     public static String askForText(String text) {
         String s = JOptionPane.showInputDialog(Main.parent, text, tr("Enter text"), JOptionPane.QUESTION_MESSAGE);
-        if (s != null && !(s = s.trim()).isEmpty()) {
-            return s;
-        } else {
-            return "";
-        }
+        return s != null ? s.trim() : null;
     }
 
     /**

src/org/openstreetmap/josm/data/osm/visitor/paint/relations/MultipolygonCache.java
@@ -83,15 +83,18 @@ public final class MultipolygonCache implements DataSetListener, LayerChangeList
         if (nc != null && r != null) {
             Map<DataSet, Map<Relation, Multipolygon>> map1 = cache.get(nc);
             if (map1 == null) {
-                cache.put(nc, map1 = new ConcurrentHashMap<>());
+                map1 = new ConcurrentHashMap<>();
+                cache.put(nc, map1);
             }
             Map<Relation, Multipolygon> map2 = map1.get(r.getDataSet());
             if (map2 == null) {
-                map1.put(r.getDataSet(), map2 = new ConcurrentHashMap<>());
+                map2 = new ConcurrentHashMap<>();
+                map1.put(r.getDataSet(), map2);
             }
             multipolygon = map2.get(r);
             if (multipolygon == null || forceRefresh) {
-                map2.put(r, multipolygon = new Multipolygon(r));
+                multipolygon = new Multipolygon(r);
+                map2.put(r, multipolygon);
                 for (PolyData pd : multipolygon.getCombinedPolygons()) {
                     if (pd.selected) {
                         selectedPolyData.add(pd);

src/org/openstreetmap/josm/data/validation/tests/Addresses.java
@@ -157,7 +157,8 @@ public class Addresses extends Test {
                         number = number.trim().toUpperCase(Locale.ENGLISH);
                         List<OsmPrimitive> list = map.get(number);
                         if (list == null) {
-                            map.put(number, list = new ArrayList<>());
+                            list = new ArrayList<>();
+                            map.put(number, list);
                         }
                         list.add(p);
                     }

src/org/openstreetmap/josm/data/validation/tests/Highways.java
@@ -127,7 +127,8 @@ public class Highways extends Test {
                 if (h != w && value != null && !value.endsWith("_link")) {
                     List<Way> list = map.get(value);
                     if (list == null) {
-                        map.put(value, list = new ArrayList<>());
+                        list = new ArrayList<>();
+                        map.put(value, list);
                     }
                     list.add(h);
                 }

src/org/openstreetmap/josm/gui/MapFrame.java
@@ -132,21 +132,21 @@ public class MapFrame extends JPanel implements Destroyable, LayerChangeListener
     // Toggle dialogs
 
     /** Conflict dialog */
-    public ConflictDialog conflictDialog;
+    public final ConflictDialog conflictDialog;
     /** Filter dialog */
-    public FilterDialog filterDialog;
+    public final FilterDialog filterDialog;
     /** Relation list dialog */
-    public RelationListDialog relationListDialog;
+    public final RelationListDialog relationListDialog;
     /** Validator dialog */
-    public ValidatorDialog validatorDialog;
+    public final ValidatorDialog validatorDialog;
     /** Selection list dialog */
-    public SelectionListDialog selectionListDialog;
+    public final SelectionListDialog selectionListDialog;
     /** Properties dialog */
-    public PropertiesDialog propertiesDialog;
+    public final PropertiesDialog propertiesDialog;
     /** Map paint dialog */
-    public MapPaintDialog mapPaintDialog;
+    public final MapPaintDialog mapPaintDialog;
     /** Notes dialog */
-    public NotesDialog noteDialog;
+    public final NotesDialog noteDialog;
 
     // Map modes
 
@@ -237,10 +237,15 @@ public class MapFrame extends JPanel implements Destroyable, LayerChangeListener
         mapView.setMinimumSize(new Dimension(10, 0));
 
         // toolBarActions, map mode buttons
-        addMapMode(new IconToggleButton(mapModeSelect = new SelectAction(this)));
-        addMapMode(new IconToggleButton(mapModeSelectLasso = new LassoModeAction(), true));
-        addMapMode(new IconToggleButton(mapModeDraw = new DrawAction(this)));
-        addMapMode(new IconToggleButton(mapModeZoom = new ZoomAction(this)));
+        mapModeSelect = new SelectAction(this);
+        mapModeSelectLasso = new LassoModeAction();
+        mapModeDraw = new DrawAction(this);
+        mapModeZoom = new ZoomAction(this);
+
+        addMapMode(new IconToggleButton(mapModeSelect));
+        addMapMode(new IconToggleButton(mapModeSelectLasso, true));
+        addMapMode(new IconToggleButton(mapModeDraw));
+        addMapMode(new IconToggleButton(mapModeZoom));
         addMapMode(new IconToggleButton(new DeleteAction(this), true));
         addMapMode(new IconToggleButton(new ParallelWayAction(this), true));
         addMapMode(new IconToggleButton(new ExtrudeAction(this), true));
@@ -250,19 +255,28 @@ public class MapFrame extends JPanel implements Destroyable, LayerChangeListener
 
         // toolBarToggles, toggle dialog buttons
         LayerListDialog.createInstance(this);
+        propertiesDialog = new PropertiesDialog();
+        selectionListDialog = new SelectionListDialog();
+        relationListDialog = new RelationListDialog();
+        conflictDialog = new ConflictDialog();
+        validatorDialog = new ValidatorDialog();
+        filterDialog = new FilterDialog();
+        mapPaintDialog = new MapPaintDialog();
+        noteDialog = new NotesDialog();
+
         addToggleDialog(LayerListDialog.getInstance());
-        addToggleDialog(propertiesDialog = new PropertiesDialog());
-        addToggleDialog(selectionListDialog = new SelectionListDialog());
-        addToggleDialog(relationListDialog = new RelationListDialog());
+        addToggleDialog(propertiesDialog);
+        addToggleDialog(selectionListDialog);
+        addToggleDialog(relationListDialog);
         addToggleDialog(new MinimapDialog());
         addToggleDialog(new CommandStackDialog());
         addToggleDialog(new UserListDialog());
-        addToggleDialog(conflictDialog = new ConflictDialog());
-        addToggleDialog(validatorDialog = new ValidatorDialog());
-        addToggleDialog(filterDialog = new FilterDialog());
+        addToggleDialog(conflictDialog);
+        addToggleDialog(validatorDialog);
+        addToggleDialog(filterDialog);
         addToggleDialog(new ChangesetDialog(), true);
-        addToggleDialog(mapPaintDialog = new MapPaintDialog());
-        addToggleDialog(noteDialog = new NotesDialog());
+        addToggleDialog(mapPaintDialog);
+        addToggleDialog(noteDialog);
         toolBarToggle.setFloatable(false);
 
         // status line below the map
@@ -271,8 +285,12 @@ public class MapFrame extends JPanel implements Destroyable, LayerChangeListener
 
         boolean unregisterTab = Shortcut.findShortcut(KeyEvent.VK_TAB, 0) != null;
         if (unregisterTab) {
-            for (JComponent c: allDialogButtons) c.setFocusTraversalKeysEnabled(false);
-            for (JComponent c: allMapModeButtons) c.setFocusTraversalKeysEnabled(false);
+            for (JComponent c: allDialogButtons) {
+                c.setFocusTraversalKeysEnabled(false);
+            }
+            for (JComponent c: allMapModeButtons) {
+                c.setFocusTraversalKeysEnabled(false);
+            }
         }
 
         if (Main.pref.getBoolean("debug.advanced-keypress-detector.enable", true)) {

commit 002485c0dba48926b91bd331c8eb7f9315cdbda3

fix NPE - regression from r10179
git-svn-id: https://josm.openstreetmap.de/svn/trunk@10180 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/preferences/server/OAuthAuthenticationPreferencesPanel.java
@@ -45,13 +45,13 @@ import org.openstreetmap.josm.tools.UserCancelException;
  * @since 2745
  */
 public class OAuthAuthenticationPreferencesPanel extends JPanel implements PropertyChangeListener {
-    private final JPanel pnlAuthorisationMessage = new JPanel(new BorderLayout());
-    private final NotYetAuthorisedPanel pnlNotYetAuthorised = new NotYetAuthorisedPanel();
-    private final AlreadyAuthorisedPanel pnlAlreadyAuthorised = new AlreadyAuthorisedPanel();
-    private final AdvancedOAuthPropertiesPanel pnlAdvancedProperties = new AdvancedOAuthPropertiesPanel();
-    private String apiUrl;
     private final JCheckBox cbShowAdvancedParameters = new JCheckBox();
     private final JCheckBox cbSaveToPreferences = new JCheckBox(tr("Save to preferences"));
+    private final JPanel pnlAuthorisationMessage = new JPanel(new BorderLayout());
+    private final NotYetAuthorisedPanel pnlNotYetAuthorised = new NotYetAuthorisedPanel();
+    private final AdvancedOAuthPropertiesPanel pnlAdvancedProperties = new AdvancedOAuthPropertiesPanel();
+    private final AlreadyAuthorisedPanel pnlAlreadyAuthorised = new AlreadyAuthorisedPanel();
+    private String apiUrl;
 
     /**
      * Create the panel