floscher/josm

commit c086f962257451c52d3f602d888c94278077fc0d

see #15182 - refactor `PlatformHookOsx` so that all GUI actions are performed in `MainApplication` and only macOS-specific stuff remains in platform hook
git-svn-id: https://josm.openstreetmap.de/svn/trunk@12695 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/tools/PlatformHookOsx.java
@@ -16,15 +16,11 @@ import java.lang.reflect.Proxy;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Executors;
+import java.util.Objects;
 
 import javax.swing.UIManager;
 
 import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.actions.OpenFileAction.OpenFileTask;
-import org.openstreetmap.josm.gui.MainApplication;
-import org.openstreetmap.josm.io.OsmTransferException;
-import org.xml.sax.SAXException;
 
 /**
  * {@code PlatformHook} implementation for Apple Mac OS X systems.
@@ -35,6 +31,8 @@ public class PlatformHookOsx implements PlatformHook, InvocationHandler {
     private static final PlatformHookOsx INVOCATION_HANDLER = new PlatformHookOsx();
 
     private String oSBuildNumber;
+
+    private NativeOsCallback osCallback;
 
     @Override
     public void preStartupHook() {
@@ -137,6 +135,11 @@ public class PlatformHookOsx implements PlatformHook, InvocationHandler {
         }
     }
 
+    @Override
+    public void setNativeOsCallback(NativeOsCallback callback) {
+        osCallback = Objects.requireNonNull(callback);
+    }
+
     @SuppressWarnings("unchecked")
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -149,22 +152,7 @@ public class PlatformHookOsx implements PlatformHook, InvocationHandler {
                 try {
                     Object oFiles = args[0].getClass().getMethod("getFiles").invoke(args[0]);
                     if (oFiles instanceof List) {
-                        Executors.newSingleThreadExecutor(Utils.newThreadFactory("openFiles-%d", Thread.NORM_PRIORITY)).submit(
-                                new OpenFileTask((List<File>) oFiles, null) {
-                            @Override
-                            protected void realRun() throws SAXException, IOException, OsmTransferException {
-                                // Wait for JOSM startup is advanced enough to load a file
-                                while (Main.parent == null || !Main.parent.isVisible()) {
-                                    try {
-                                        Thread.sleep(25);
-                                    } catch (InterruptedException e) {
-                                        Logging.warn(e);
-                                        Thread.currentThread().interrupt();
-                                    }
-                                }
-                                super.realRun();
-                            }
-                        });
+                        osCallback.openFiles((List<File>) oFiles);
                     }
                 } catch (ReflectiveOperationException | SecurityException | IllegalArgumentException ex) {
                     Logging.warn("Failed to access open files event: " + ex);
@@ -172,7 +160,7 @@ public class PlatformHookOsx implements PlatformHook, InvocationHandler {
             }
             break;
         case "handleQuitRequestWith":
-            boolean closed = MainApplication.exitJosm(false, 0, null);
+            boolean closed = osCallback.handleQuitRequest();
             if (args[1] != null) {
                 try {
                     args[1].getClass().getDeclaredMethod(closed ? "performQuit" : "cancelQuit").invoke(args[1]);
@@ -184,10 +172,10 @@ public class PlatformHookOsx implements PlatformHook, InvocationHandler {
             }
             break;
         case "handleAbout":
-            MainApplication.getMenu().about.actionPerformed(null);
+            osCallback.handleAbout();
             break;
         case "handlePreferences":
-            MainApplication.getMenu().preferences.actionPerformed(null);
+            osCallback.handlePreferences();
             break;
         default:
             Logging.warn("OSX unsupported method: "+method.getName());

src/org/openstreetmap/josm/gui/MainApplication.java
@@ -37,6 +37,7 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
@@ -59,6 +60,7 @@ import org.openstreetmap.gui.jmapviewer.FeatureAdapter;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.actions.OpenFileAction;
+import org.openstreetmap.josm.actions.OpenFileAction.OpenFileTask;
 import org.openstreetmap.josm.actions.PreferencesAction;
 import org.openstreetmap.josm.actions.RestartAction;
 import org.openstreetmap.josm.actions.downloadtasks.DownloadGpsTask;
@@ -102,6 +104,7 @@ import org.openstreetmap.josm.io.OnlineResource;
 import org.openstreetmap.josm.io.OsmApi;
 import org.openstreetmap.josm.io.OsmApiInitializationException;
 import org.openstreetmap.josm.io.OsmTransferCanceledException;
+import org.openstreetmap.josm.io.OsmTransferException;
 import org.openstreetmap.josm.io.auth.CredentialsManager;
 import org.openstreetmap.josm.io.auth.DefaultAuthenticator;
 import org.openstreetmap.josm.io.protocols.data.Handler;
@@ -116,6 +119,7 @@ import org.openstreetmap.josm.tools.Logging;
 import org.openstreetmap.josm.tools.OpenBrowser;
 import org.openstreetmap.josm.tools.OsmUrlToBounds;
 import org.openstreetmap.josm.tools.OverpassTurboQueryWizard;
+import org.openstreetmap.josm.tools.PlatformHook.NativeOsCallback;
 import org.openstreetmap.josm.tools.PlatformHookWindows;
 import org.openstreetmap.josm.tools.RightAndLefthandTraffic;
 import org.openstreetmap.josm.tools.Shortcut;
@@ -123,6 +127,7 @@ import org.openstreetmap.josm.tools.Territories;
 import org.openstreetmap.josm.tools.Utils;
 import org.openstreetmap.josm.tools.bugreport.BugReport;
 import org.openstreetmap.josm.tools.bugreport.BugReportExceptionHandler;
+import org.xml.sax.SAXException;
 
 /**
  * Main window class application.
@@ -724,6 +729,7 @@ public class MainApplication extends Main {
 
         // initialize the platform hook, and
         Main.determinePlatformHook();
+        Main.platform.setNativeOsCallback(new DefaultNativeOsCallback());
         // call the really early hook before we do anything else
         Main.platform.preStartupHook();
 
@@ -1185,6 +1191,43 @@ public class MainApplication extends Main {
                         ));
             }
             return false;
+        }
+    }
+
+    private static class DefaultNativeOsCallback implements NativeOsCallback {
+        @Override
+        public void openFiles(List<File> files) {
+            Executors.newSingleThreadExecutor(Utils.newThreadFactory("openFiles-%d", Thread.NORM_PRIORITY)).submit(
+                    new OpenFileTask(files, null) {
+                @Override
+                protected void realRun() throws SAXException, IOException, OsmTransferException {
+                    // Wait for JOSM startup is advanced enough to load a file
+                    while (Main.parent == null || !Main.parent.isVisible()) {
+                        try {
+                            Thread.sleep(25);
+                        } catch (InterruptedException e) {
+                            Logging.warn(e);
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    super.realRun();
+                }
+            });
+        }
+
+        @Override
+        public boolean handleQuitRequest() {
+            return MainApplication.exitJosm(false, 0, null);
+        }
+
+        @Override
+        public void handleAbout() {
+            MainApplication.getMenu().about.actionPerformed(null);
+        }
+
+        @Override
+        public void handlePreferences() {
+            MainApplication.getMenu().preferences.actionPerformed(null);
         }
     }
 }

src/org/openstreetmap/josm/tools/PlatformHook.java
@@ -271,4 +271,43 @@ public interface PlatformHook {
                     DateUtils.getDateFormat(DateFormat.MEDIUM).format(expiration), false);
         }
     }
+
+    /**
+     * Called when interfacing with native OS functions. Currently only used with macOS.
+     * The callback must perform all GUI-related tasks associated to an OS request.
+     * The non-GUI, platform-specific tasks, are usually performed by the {@code PlatformHook}.
+     * @since 12695
+     */
+    interface NativeOsCallback {
+        /**
+         * macOS: Called when JOSM is asked to open a list of files.
+         * @param files list of files to open
+         */
+        void openFiles(List<File> files);
+
+        /**
+         * macOS: Invoked when JOSM is asked to quit.
+         * @return {@code true} if JOSM has been closed, {@code false} if the user has cancelled the operation.
+         */
+        boolean handleQuitRequest();
+
+        /**
+         * macOS: Called when JOSM is asked to show it's about dialog.
+         */
+        void handleAbout();
+
+        /**
+         * macOS: Called when JOSM is asked to show it's preferences UI.
+         */
+        void handlePreferences();
+    }
+
+    /**
+     * Registers the native OS callback. Currently only needed for macOS.
+     * @param callback the native OS callback
+     * @since 12695
+     */
+    default void setNativeOsCallback(NativeOsCallback callback) {
+        // To be implemented if needed
+    }
 }

commit 16e83106ef82abf385302438dc504c56b00c348a

fix #15207 - NPE at PlatformHookOsx.invoke when opening preferences (regression from r12695)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@12697 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/tools/PlatformHookOsx.java
@@ -28,8 +28,6 @@ import org.openstreetmap.josm.Main;
  */
 public class PlatformHookOsx implements PlatformHook, InvocationHandler {
 
-    private static final PlatformHookOsx INVOCATION_HANDLER = new PlatformHookOsx();
-
     private String oSBuildNumber;
 
     private NativeOsCallback osCallback;
@@ -58,7 +56,7 @@ public class PlatformHookOsx implements PlatformHook, InvocationHandler {
             Class<?> openFilesHandler = findHandlerClass("OpenFilesHandler");
             Class<?> preferencesHandler = findHandlerClass("PreferencesHandler");
             Object proxy = Proxy.newProxyInstance(PlatformHookOsx.class.getClassLoader(), new Class<?>[] {
-                quitHandler, aboutHandler, openFilesHandler, preferencesHandler}, INVOCATION_HANDLER);
+                quitHandler, aboutHandler, openFilesHandler, preferencesHandler}, this);
             Object appli = eawtApplication.getConstructor((Class[]) null).newInstance((Object[]) null);
             if (Utils.getJavaVersion() >= 9) {
                 setHandlers(Desktop.class, quitHandler, aboutHandler, openFilesHandler, preferencesHandler, proxy, Desktop.getDesktop());