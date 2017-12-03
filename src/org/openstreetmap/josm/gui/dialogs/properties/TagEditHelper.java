floscher/josm

commit f9acd5c54bfefbffd456ea630ecb76ddf8933bd1

fix #13747 - do not select all characters from a combobox when switching between JOSM and another application
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13139 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/properties/TagEditHelper.java
@@ -634,13 +634,13 @@ public class TagEditHelper {
            FocusAdapter focus = new FocusAdapter() {
                @Override
                public void focusGained(FocusEvent e) {
-                   String key = keys.getEditor().getItem().toString();
-
-                   List<AutoCompletionItem> valueList = autocomplete.getTagValues(getAutocompletionKeys(key), comparator);
                    Logging.trace("Focus gained by {0}, e={1}", values, e);
-                   values.setPossibleAcItems(valueList);
-                   values.getEditor().selectAll();
-                   objKey = key;
+                   String key = keys.getEditor().getItem().toString();
+                   if (!Objects.equals(key, objKey)) {
+                       values.setPossibleAcItems(autocomplete.getTagValues(getAutocompletionKeys(key), comparator));
+                       values.getEditor().selectAll();
+                       objKey = key;
+                   }
                }
            };
            editor.addFocusListener(focus);

src/org/openstreetmap/josm/gui/tagging/ac/AutoCompletingComboBox.java
@@ -75,7 +75,9 @@ public class AutoCompletingComboBox extends JosmComboBox<AutoCompletionItem> {
                 if (old != null) {
                     sysSel.setContents(old, null);
                 }
-            } else {
+            } else if (e != null && e.getOppositeComponent() != null) {
+                // Select all characters when the change of focus occurs inside JOSM only.
+                // When switching from another application, it is annoying, see #13747
                 editorComponent.selectAll();
             }
         }

commit 516f2ad5d372df156d466c64baff2c6c35bd0704

fix #15627 - autocompletion values not correctly reset on change of focus (regression from r13139)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13190 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/properties/TagEditHelper.java
@@ -42,6 +42,7 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
+import javax.swing.ComboBoxModel;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBoxMenuItem;
@@ -636,8 +637,17 @@ public class TagEditHelper {
                public void focusGained(FocusEvent e) {
                    Logging.trace("Focus gained by {0}, e={1}", values, e);
                    String key = keys.getEditor().getItem().toString();
+                   List<AutoCompletionItem> correctItems = autocomplete.getTagValues(getAutocompletionKeys(key), comparator);
+                   ComboBoxModel<AutoCompletionItem> currentModel = values.getModel();
+                   final int size = correctItems.size();
+                   boolean valuesOK = size == currentModel.getSize();
+                   for (int i = 0; valuesOK && i < size; i++) {
+                       valuesOK = Objects.equals(currentModel.getElementAt(i), correctItems.get(i));
+                   }
+                   if (!valuesOK) {
+                       values.setPossibleAcItems(correctItems);
+                   }
                    if (!Objects.equals(key, objKey)) {
-                       values.setPossibleAcItems(autocomplete.getTagValues(getAutocompletionKeys(key), comparator));
                        values.getEditor().selectAll();
                        objKey = key;
                    }